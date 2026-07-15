4492bbc31deeba855b49a15ef5be5b37a58f2527
malaikahm <mayaomegas201@gmail.com>
2026-07-15 02:40:24 -1200

    compteur

---
+++ compteur/compt.js
@@ -0,0 +1,23 @@
+let compteur = 0; 
+//pour remetre la valeur du compteur a 0 
+function décroissant() {
+    compteur --;
+    document.getElementById("result").innerText= compteur; 
+    document.getElementById("result").style.color = "red"
+}
+
+function croissant() {
+    compteur ++;
+    document.getElementById("result").innerText= compteur; 
+    document.getElementById("result").style.color = "green"
+
+}
+
+function restaurer() {
+    compteur =0;
+    document.getElementById("result").innerText= compteur; 
+    document.getElementById("result").style.color = "black"
+}
+document.getElementById("décroissant").addEventListener("click", décroissant)
+document.getElementById("croissant").addEventListener("click", croissant)
+document.getElementById("restaurer").addEventListener("click", restaurer)
